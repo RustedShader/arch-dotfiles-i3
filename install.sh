49ab2ac00c2154f9a98e2daa3883560a1f9fe503
RustedShader <1234568dhj@gmail.com>
2023-04-14 17:07:04 +0530

    main code

--- install.sh
+++ install.sh
@@ -14,9 +14,15 @@ printf "Setting up Parallel Downloads to 5 ... \n"
 sudo bash -c 'sed -i "s/#ParallelDownloads = 5/ParallelDownloads = 5/g" /etc/pacman.conf'
 sleep 1
 
+### INSTALL Git and Make ###
+printf "Installing git and make...\n"
+sudo pacman --noconfirm -S git make
+
+### SETTING MAKEFLAGS TO USE ALL CORES ###
+printf "Setting up MAKEFLAGS...\n"
+sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j$(nproc)"/g' /etc/makeflags.conf
+
 ### INSTALL YAY ###
-printf "Installing git...\n"
-sudo pacman --noconfirm -S git
 printf "Installing yay...\n"
 git clone https://aur.archlinux.org/yay.git && cd ./yay && makepkg -si --noconfirm && cd $OLDPWD && rm -rf ./yay
 sleep 1
