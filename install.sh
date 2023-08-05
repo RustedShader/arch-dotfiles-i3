457bd0acf2d6450c1067538f12458f125b637880
Shubhang Sharma <69749260+RustedShader@users.noreply.github.com>
2023-08-06 03:56:54 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -42,7 +42,7 @@ printf "Are You NVIDIA User ? \n Press y for yes or n for No \n"
 read nvidia
 if [[ "$nvidia" == y ]];
 then
-yay -sudoloop --noconfirm -S nvidia nvidia-utils nvidia-settings
+yay --noconfirm -S nvidia nvidia-utils nvidia-settings
 else
 printf "Nothing to Do Here \n"
 fi
@@ -53,7 +53,7 @@ read asus
 
 if [[ "$asus" == y ]];
 then
-yay -sudoloop --noconfirm -S rog-control-center
+yay --noconfirm -S rog-control-center
 ### ENABLE ASUSCTL ###
 printf "Enabling Asusctl"
 sudo systemctl enable --now power-profiles-daemon.service
