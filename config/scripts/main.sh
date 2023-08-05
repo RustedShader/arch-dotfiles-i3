8fa7c03adfb6cec3cd97c9a7169358ea19c7cc9a
Shubhang Sharma <69749260+RustedShader@users.noreply.github.com>
2023-08-06 03:56:25 +0530

    Update main.sh

--- config/scripts/main.sh
+++ config/scripts/main.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
 wallpaper=$(ls $HOME/.config/scripts/active_wallpaper | sed -n 1\p)
-wal -i $HOME/.config/scripts/active_wallpaper/$wallpaper && feh --bg-scale $HOME/.config/scripts/active_wallpaper/$wallpaper && cp $HOME/.cache/wal/colors-rofi-dark.rasi $HOME/.config/rofi ; echo "ROFI Config Exported !" && cp $HOME/.cache/wal/dunstrc $HOME/.config/dunst/dunstrc ; echo "Dunstrc exported !" && notify-send "Theme Chaged & Configs Updated !"
+wal -i $HOME/.config/scripts/active_wallpaper/$wallpaper && feh --bg-scale $HOME/.config/scripts/active_wallpaper/$wallpaper && cp $HOME/.cache/wal/colors-rofi-dark.rasi $HOME/.config/rofi ; echo "ROFI Config Exported !" && cp $HOME/.cache/wal/dunstrc $HOME/.config/dunst/dunstrc ; echo "Dunstrc exported !" && notify-send "Theme Chaged & Configs Updated !" && $HOME/.config/scripts/./alacritty_pywal.sh
 
 
